0fed2b071039f8a38770ae4362b8b6c06caf68a5
visioned <ajkg11@gmail.com>
2023-02-15 00:36:21 -0800

    font

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -22,11 +22,11 @@ function Rating({
     <form onSubmit={handleFormSubmitted} className="flex">
       <div className="p-[30px] text-white h-[400px] w-[400px] bg-[#232934] rounded-3xl">
         <img
-          className="bg-[#353f47] rounded-full h-[48px] w-[48px] object-scale-down mb-[26px] text-bold"
+          className="bg-[#353f47] rounded-full h-[48px] w-[48px] object-scale-down mb-[26px]"
           src={star}
           alt=""
         />
-        <h1 className="mb-[16px] text-3xl">How did we do?</h1>
+        <h1 className="mb-[16px] text-3xl font-bold">How did we do?</h1>
         <p className="mb-[26px] text-[#7c8798] tracking-tight">
           Please let us know how we did with your support request. All feed back
           is appreciated to help us improve our offering!
